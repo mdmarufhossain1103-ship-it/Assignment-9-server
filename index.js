4ca380a2916115358991653d37ad92111729b343
mdmarufhossain1103-ship-it <mdmarufhossain1103@gmail.com>
2026-05-28 16:38:06 +0600

    Complete Assignment

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
 
-        await client.connect();
+        // await client.connect();
 
         const db = client.db('ideavault');
 
@@ -369,7 +369,7 @@ async function run() {
             }
         });
 
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
 
         console.log("MongoDB Connected Successfully");
 
